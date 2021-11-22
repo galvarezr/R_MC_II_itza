ef35255a441e45e542cf683101d431733647a07a
Eduardo Muniz <e@munizt.mx>
2021-11-21 23:52:36 -0600

    Pruebas de sincronización en git

--- MainCode.r
+++ MainCode.r
@@ -1,2 +1,23 @@
+## Trabajo final MAPP Otoño. Métodos cuantitativos.
+## Integrantes:
+## Guadalupe Álvarez
+## Ángel Carrillo
+## Eduardo Muñiz
+
+##### Paquetería ----
+library(cluster)
+library(dplyr)
+library(pacman)
+library(ggplot2)
+library(factoextra)
+
+##### Base de datos ----
+computoeinegi <- read.csv("2021 computoeinegi.csv")
+str(computoeinegi)
+
+View(computoeinegi)
+
+
+
 print("Hello World!")
 1+1
